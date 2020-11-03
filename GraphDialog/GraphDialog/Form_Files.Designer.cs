26eae209b0fefd63f17fc1f7945a4047f0b9370b
p85kmax <p85kmax@gmail.com>
2020-11-03 11:10:13 +0900

    fils

--- GraphDialog/GraphDialog/Form_Files.Designer.cs
+++ GraphDialog/GraphDialog/Form_Files.Designer.cs
@@ -28,12 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.button_Path = new System.Windows.Forms.Button();
+            this.button_FileNameChange = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(1, -1);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(416, 428);
+            this.treeView1.TabIndex = 0;
+            // 
+            // button_Path
+            // 
+            this.button_Path.Location = new System.Drawing.Point(433, 12);
+            this.button_Path.Name = "button_Path";
+            this.button_Path.Size = new System.Drawing.Size(75, 28);
+            this.button_Path.TabIndex = 1;
+            this.button_Path.Text = "Path";
+            this.button_Path.UseVisualStyleBackColor = true;
+            this.button_Path.Click += new System.EventHandler(this.button_Path_Click);
+            // 
+            // button_FileNameChange
+            // 
+            this.button_FileNameChange.Location = new System.Drawing.Point(423, 85);
+            this.button_FileNameChange.Name = "button_FileNameChange";
+            this.button_FileNameChange.Size = new System.Drawing.Size(84, 35);
+            this.button_FileNameChange.TabIndex = 2;
+            this.button_FileNameChange.Text = "fileName";
+            this.button_FileNameChange.UseVisualStyleBackColor = true;
+            this.button_FileNameChange.Click += new System.EventHandler(this.button_FileNameChange_Click);
+            // 
+            // Form_Files
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(520, 427);
+            this.Controls.Add(this.button_FileNameChange);
+            this.Controls.Add(this.button_Path);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form_Files";
             this.Text = "Form_Files";
+            this.Load += new System.EventHandler(this.Form_Files_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button_Path;
+        private System.Windows.Forms.Button button_FileNameChange;
     }
 }
--- GraphDialog/GraphDialog/Form_Files.cs
+++ GraphDialog/GraphDialog/Form_Files.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+
 
 namespace GraphDialog
 {
@@ -17,9 +19,31 @@ namespace GraphDialog
             InitializeComponent();
         }
 
+        private void ListDirectory(TreeView treeView, string path)
+        {
+            treeView.Nodes.Clear();
+            var rootDirectoryInfo = new DirectoryInfo(path);
+            treeView.Nodes.Add(CreateDirectoryNode(rootDirectoryInfo));
+        }
+
+        private TreeNode CreateDirectoryNode(DirectoryInfo directoryInfo)
+        {
+            var directoryNode = new TreeNode(directoryInfo.Name);
+            foreach (var directory in directoryInfo.GetDirectories())
+                directoryNode.Nodes.Add(CreateDirectoryNode(directory));
+            foreach (var file in directoryInfo.GetFiles())
+                directoryNode.Nodes.Add(new TreeNode(file.Name));
+            return directoryNode;
+        }
+
+        private void Form_Files_Load(object sender, EventArgs e)
+        {
+            ListDirectory(treeView1, @"d:\");
+        }
 
         private void button_PairLoad_Click(object sender, EventArgs e)
         {
+            //FolderBrowserDialog 보다 괜찮은 폴더 선택 화면 -CommonOpenFileDialog
             ////FolderBrowserDialog 
 
             //OpenFileDialog ofdFile = new OpenFileDialog();
@@ -78,5 +102,72 @@ namespace GraphDialog
             //Path.GetDirectoryName(fullName);              //c:\\folder\\
 
         }
+
+        private void button_Path_Click(object sender, EventArgs e)
+        {
+            //FolderBrowserDialog 보다 괜찮은 폴더 선택 화면 -CommonOpenFileDialog
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                var folderPath=dlg.SelectedPath.ToString();
+                ListDirectory(treeView1, folderPath);
+            }
+
+        }
+
+        private void button_FileNameChange_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                var folderPath = dlg.SelectedPath.ToString();
+                ChangeFileNameFromDirectory(new DirectoryInfo(folderPath));
+            }
+        }
+        private TreeNode ChangeFileNameFromDirectory(DirectoryInfo directoryInfo)
+        {
+            var directoryNode = new TreeNode(directoryInfo.Name);
+            foreach (var directory in directoryInfo.GetDirectories())
+                directoryNode.Nodes.Add(ChangeFileNameFromDirectory(directory));
+            foreach (var file in directoryInfo.GetFiles())
+            {
+               if(file.Name == "MainApp.cs")
+                {
+                    //var lastFolder = Path.GetDirectoryName(file.FullName);
+                    //var pathWithoutLastFolder = Path.GetDirectoryName(lastFolder);
+                    var lastfolder2= parentFolderName(file.FullName);
+                    var parent = Directory.GetParent(Path.GetDirectoryName(file.FullName));
+                    
+
+                    //Path.GetFileName(fullName);                   //test.txt
+                    //Path.GetFileNameWithoutExtension(fullName);   //test
+                    //Path.GetExtension(fullName);                  //.txt
+                    //Path.GetPathRoot(fullName);                   //c:\
+                    string dir =Path.GetDirectoryName(file.FullName);              //c:\\folder\\
+                    string ext =Path.GetExtension(file.FullName);                  //.txt
+
+                    Path.Combine(dir, lastfolder2 + ext);
+                    File.Copy(file.FullName, Path.Combine(parent.FullName, lastfolder2 + ".txt"));
+
+                }
+            }
+            return directoryNode;
+        }
+
+        string parentFolderName(string path)
+        {
+            DirectoryInfo parentDir = Directory.GetParent(path);
+            string dir = Path.GetDirectoryName(path);
+            var folders = dir.Split('\\');
+            var lastfolderName = folders.Last();
+            // or possibly
+            // DirectoryInfo parentDir = Directory.GetParent(path.EndsWith("\\") ? path : string.Concat(path, "\\"));
+            // The result is available here
+            //var myParentDir = parentDir.Parent.FullName;
+            return lastfolderName;
+        }
+
     }
 }
