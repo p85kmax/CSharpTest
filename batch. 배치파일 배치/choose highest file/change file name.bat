81968aa547ab17eace7893a646465e634e0371cb
p85kmax <p85kmax@gmail.com>
2022-06-26 14:55:43 +0900

    파일이름 바꾸기 good

--- batch. 배치파일 배치/choose highest file/change file name.bat
+++ batch. 배치파일 배치/choose highest file/change file name.bat
@@ -4,25 +4,40 @@ setlocal enabledelayedexpansion
 set x=7
 echo %x%
 set /a x+=1
-echo %x%	
+echo %x%
 set /a x+=1
 		echo %x%
 set /a x+=1
 echo %x%
-
+set /a x=7
 
 for %%a in ( *.mmm ) do (
 	echo %%a
 	echo ............
 	for /f "delims=. tokens=1,2" %%b in ("%%a") do (
-	    set /a "x+=1"
-		::%x% 랑 !x!랑 다름. 후자가 정상 동작
-		echo %x% 
-		echo __
-		echo [!x!] [%%b]  [%%c]
-		echo %%a ---!x!%%b.%%c
+	    rem %x% 랑 !x!랑 다름. 후자가 정상 동작
+
+		if !x! lss 10 ( 
+			set prefix=000!x!
+		) else (
+			if !x! lss 100 ( set prefix=00!x!
+			) else (
+				if !x! lss 1000 set prefix=0!x!
+				) 
+			)
+		)
+		
+		set /a "x*=10"
+				
+				
+	    echo [!prefix!]
+	    rem %x% 랑 !x!랑 다름. 후자가 정상 동작
+		rem echo %x% 
+		rem echo __
+		rem echo [!x!] [%%b]  [%%c]
+		rem echo %%a ---!x!%%b.%%c
 		echo !x!%%a  ////
-		rename %%a !x!%%a
+		rem rename %%a !x!%%a
 	)
 )
 pause > nul
