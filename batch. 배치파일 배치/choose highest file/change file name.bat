ccefadb4ee9101e66b702fd5da4161cbd599f893
p85kmax <p85kmax@gmail.com>
2022-06-26 15:13:00 +0900

    s

--- batch. 배치파일 배치/choose highest file/change file name.bat
+++ batch. 배치파일 배치/choose highest file/change file name.bat
@@ -1,5 +1,16 @@
 @echo off
 setlocal enabledelayedexpansion
+:: Enableextensions / Disableextensions 
+
+:: Enabledelayedexpansion / Disabledelayedexpasion
+::언뜻 보면 정말... 어려워 보이지만.. 이것은 C언어에서 헤더파일과 거의 같은 계념입니다.
+::C에서는 기본적으로 입력, 출력이 가능한 #include <stdio.h> 를 사용하잖아요?
+::배치파일 또한 변수 표현을 더 확장시켜 주시 위해서 필요하다고 보면 되겠습니다.
+::배치파일에서 A라는 변수에 10을 넣었을때, 출력을 하기 위해서는
+::echo %A% 라고 우리는 항상 이렇게 써왔습니다.
+::하지만 변수 확장을 한다면, echo !A! 라고 표현을 하면 됩니다.
+
+
 ::for /f %%a in (*.txt) do echo %%a
 set x=7
 echo %x%
@@ -41,3 +52,4 @@ for %%a in ( *.mmm ) do (
 	)
 )
 pause > nul
+setlocal Disableextensions
